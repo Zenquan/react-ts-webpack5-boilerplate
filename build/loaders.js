7b3b15684c2f3b85977674804f16e677b550ca1a
Zenquan <1847867150@qq.com>
2022-04-10 02:39:32 +0800

    feat: 新增cache-loader配置

--- build/loaders.js
+++ build/loaders.js
@@ -1,10 +1,49 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { resolve } = require('./utils');
 
 const loaders = [
   {
     test: /\.(js|jsx)$/,
-    use: ['cache-loader', 'babel-loader'],
+    use: [
+      'cache-loader',
+      {
+        loader: 'thread-loader',
+        options: {
+          // the number of spawned workers, defaults to (number of cpus - 1) or
+          // fallback to 1 when require('os').cpus() is undefined
+          workers: 4,
+
+          // number of jobs a worker processes in parallel
+          // defaults to 20
+          workerParallelJobs: 50,
+
+          // additional node.js arguments
+          workerNodeArgs: ['--max-old-space-size=1024'],
+
+          // Allow to respawn a dead worker pool
+          // respawning slows down the entire compilation
+          // and should be set to false for development
+          poolRespawn: false,
+
+          // timeout for killing the worker processes when idle
+          // defaults to 500 (ms)
+          // can be set to Infinity for watching builds to keep workers alive
+          poolTimeout: 2000,
+
+          // number of jobs the poll distributes to the workers
+          // defaults to 200
+          // decrease of less efficient but more fair distribution
+          poolParallelJobs: 50,
+
+          // name of the pool
+          // can be used to create different pools with elsewise identical options
+          name: 'my-pool',
+        },
+      },
+      'babel-loader',
+    ],
     exclude: /node_modules/,
+    include: [resolve('src')],
   },
   {
     test: /\.css$/,
@@ -66,8 +105,46 @@ const loaders = [
   },
   {
     test: /\.ts(x)?$/,
-    loader: 'ts-loader',
+    use: [
+      'cache-loader',
+      // {
+      //   loader: 'thread-loader',
+      //   options: {
+      //     // the number of spawned workers, defaults to (number of cpus - 1) or
+      //     // fallback to 1 when require('os').cpus() is undefined
+      //     workers: 2,
+
+      //     // number of jobs a worker processes in parallel
+      //     // defaults to 20
+      //     workerParallelJobs: 50,
+
+      //     // additional node.js arguments
+      //     workerNodeArgs: ['--max-old-space-size=1024'],
+
+      //     // Allow to respawn a dead worker pool
+      //     // respawning slows down the entire compilation
+      //     // and should be set to false for development
+      //     poolRespawn: false,
+
+      //     // timeout for killing the worker processes when idle
+      //     // defaults to 500 (ms)
+      //     // can be set to Infinity for watching builds to keep workers alive
+      //     poolTimeout: 2000,
+
+      //     // number of jobs the poll distributes to the workers
+      //     // defaults to 200
+      //     // decrease of less efficient but more fair distribution
+      //     poolParallelJobs: 50,
+
+      //     // name of the pool
+      //     // can be used to create different pools with elsewise identical options
+      //     name: 'my-pool',
+      //   },
+      // },
+      'ts-loader',
+    ],
     exclude: /node_modules/,
+    include: [resolve('src')],
   },
   {
     test: /\.less$/,
