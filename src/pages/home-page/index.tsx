17de2526f7d08bf998df44dac2ff71f3076c5d52
Zenquan <1847867150@qq.com>
2022-04-12 22:26:15 +0800

    refactor: 修改首页页面

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 // import useSWR from 'swr';
+import logo from '/@/assets/logo.svg';
 import style from './index.module.less';
 
 const Home: FC = () => {
@@ -9,7 +10,12 @@ const Home: FC = () => {
   // if (error) return <div>failed to load</div>;
   // if (!data) return <div>loading...</div>;
 
-  return <div className={style.home}>Hello, React!</div>;
+  return (
+    <div className={style.home}>
+      <img src={logo} alt="" className={style.logo} />
+      <p>Hello, React!</p>
+    </div>
+  );
 };
 
 export default Home;
